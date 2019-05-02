5226950035a12befcffa0078a47b91e808f9c84f
Daniel Ale <denielale9291@gmail.com>
2019-05-02 08:30:46 +0100

    Updates

---
+++ precache-manifest.db5952e15590b23fffe19d95b0b20405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c491a0cc4ce1136dc518ffbc18a2a5",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "8ca374d3e9b42208e60e",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "38fb20d4f3a42a7bb62d",
+    "url": "/contact-manager/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "8ca374d3e9b42208e60e",
+    "url": "/contact-manager/static/js/2.077e0b76.chunk.js"
+  },
+  {
+    "revision": "38fb20d4f3a42a7bb62d",
+    "url": "/contact-manager/static/js/main.2e4116b2.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
